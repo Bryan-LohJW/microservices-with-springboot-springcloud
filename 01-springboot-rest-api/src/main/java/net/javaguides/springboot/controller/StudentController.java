cd623d0e42ca5ddd2c3964219e02ba6dd087fac9
Bryan Personal <bryanlohjw97@gmail.com>
2023-07-19 06:49:06 +0800

    post put delete mapping with  request body

--- 01-springboot-rest-api/src/main/java/net/javaguides/springboot/controller/StudentController.java
+++ 01-springboot-rest-api/src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -1,9 +1,8 @@
 package net.javaguides.springboot.controller;
 
 import net.javaguides.springboot.bean.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,9 +31,45 @@ public class StudentController {
 
     // Spring Boot REST API with Path Variable
     // {id} - URI template variable
-    // http//localhost:8080/students/1/Ramesh/Fadatare
+    // http://localhost:8080/students/1/Ramesh/Fadatare
     @GetMapping("students/{id}/{first-name}/{last-name}")
     public Student studentPathVariable(@PathVariable("id") int studentId, @PathVariable("first-name") String firstName, @PathVariable("last-name") String lastName) {
         return new Student(studentId, firstName, lastName);
+    }
+
+    // Spring Boot REST API with Request Param
+    // http://localhost:8080/students/query?id=1&firstName=Ramesh&lastName=Fadatare
+    @GetMapping("students/query")
+    public Student studentRequestVariable(@RequestParam int id, @RequestParam String firstName, @RequestParam String lastName) {
+        return new Student(id, firstName, lastName);
+    }
+
+    // Spring Boot REST API that handles HTTP POST Request - creating new resources
+    // @PostRequest and @ResponseBody
+    // @RequestBody converts the JSON from request into a Java object
+    // Spring Boot uses HTTP status 200 as a default
+    @PostMapping("students/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student) {
+        // instead of saving, will print out in console
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
+    }
+
+    // Spring Boot REST API that handles HTTP PUT Request - updating existing resources
+    @PutMapping("students/{id}/update")
+    public Student updateStudent(@RequestBody Student student, @PathVariable("id") int studentId) {
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
+    }
+
+    // Spring Boot REST API that handles HTTP DELETE Request - deleting the existing resources
+    @DeleteMapping("students/{id}/delete")
+    public String deleteStudent(@PathVariable("id") int studentId) {
+        System.out.println(studentId);
+        return "Student deleted successfully!";
     }
 }
