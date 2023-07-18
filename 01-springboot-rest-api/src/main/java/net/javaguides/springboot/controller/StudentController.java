8be164d991f5c8b13a1a8498779a7bf54646fcd4
Bryan Personal <bryanlohjw97@gmail.com>
2023-07-19 05:56:48 +0800

    return list

--- 01-springboot-rest-api/src/main/java/net/javaguides/springboot/controller/StudentController.java
+++ 01-springboot-rest-api/src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -4,15 +4,28 @@ import net.javaguides.springboot.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 
     // http://localhost:8080/student
     @GetMapping("student")
-    public Student getStudent(){
+    public Student getStudent() {
         Student student = new Student(
                 1, "Ramesh", "Fadatare"
         );
         return student;
+    }
+
+    @GetMapping("students")
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Ramesh", "Fadatare"));
+        students.add(new Student(2, "Umesh", "Fadatare"));
+        students.add(new Student(3, "Ram", "Jadhav"));
+        students.add(new Student(4, "Sanjay", "Pawar"));
+        return students;
     }
 }
