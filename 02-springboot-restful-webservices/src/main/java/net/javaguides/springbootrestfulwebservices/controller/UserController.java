cdaaf2143d56fbf419254cb4b9782f6a52ed1c89
Bryan Personal <bryanlohjw97@gmail.com>
2023-07-26 22:36:02 +0800

    update user rest api

--- 02-springboot-restful-webservices/src/main/java/net/javaguides/springbootrestfulwebservices/controller/UserController.java
+++ 02-springboot-restful-webservices/src/main/java/net/javaguides/springbootrestfulwebservices/controller/UserController.java
@@ -33,4 +33,12 @@ public class UserController {
         List<User> users = userService.getAllUsers();
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
+
+    @PutMapping("{id}")
+    public ResponseEntity<User> updateUser(@PathVariable("id") Long userId,
+                                           @RequestBody User user) {
+        user.setId(userId);
+        User updatedUser = userService.updateUser(user);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+    }
 }
